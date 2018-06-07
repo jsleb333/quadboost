c99dd5562563fd33f3dca80625d36746c011aa2f
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-06-07 16:08:21 -0400

    splitted QuadBoost from QuadBoostMH

--- quadboost.py
+++ quadboost.py
@@ -1,13 +1,13 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import Ridge
+from weak_learner import WLRidgeMH
 import matplotlib.pyplot as plt
 
 from label_encoder import LabelEncoder, OneHotEncoder, AllPairsEncoder
 from mnist_dataset import MNISTDataset
 from utils import *
 
-class QuadBoostMH:
+class QuadBoost:
     def __init__(self, weak_learner, encoder=None):
         """
         weak_learner (Object that defines the 'fit' method and the 'predict' method): Weak learner that generates weak predictors to be boosted on.
@@ -15,6 +15,7 @@ class QuadBoostMH:
         """
         self.weak_learner = weak_learner
         self.encoder = encoder
+        self.weak_predictors = []
 
 
     def fit(self, X, Y, T, f0=None):
@@ -36,8 +37,6 @@ class QuadBoostMH:
             self.f0 = f0
 
         residue = encoded_Y - self.f0
-        self.alphas = []
-        self.weak_predictors = []
 
         # Boosting algorithm
         for t in range(T):
@@ -46,37 +45,26 @@ class QuadBoostMH:
             wp_acc = accuracy_score(y_true=Y, y_pred=self.encoder.decode_labels(weak_prediction))
             print('weak predictor accuracy:' + str(wp_acc))
             print('Boosting round ' + str(t+1) + ' - train accuracy: ' + str(self.evaluate(X, Y)))
+        
+        # If the boosting algorithm uses the confidence of the WeakLearner as a weights instead of computing one, we set a weight of 1 for every weak predictor.
+        if self.weak_predictors_weights == []:
+            self.weak_predictors_weights = [1]*T
     
 
     def _boost(self, X, residue, weights):
         """
-        Implements one round of boosting. 
-        Appends the lists of alphas and of weak_predictors with the fitted weak learner.
-
-        X (Array of shape (n_examples, ...)): Examples.
-        residue (Array of shape (n_examples, encoding_dim)): Residues to fit for the examples X.
-        weights (Array of shape (n_examples, encoding_dim)): Weights of the examples X for each encoding.
-
-        Returns the calculated residue and the weak_prediction.
+        Should implements one round of boosting.
+        Must return the calculated residue and the weak_prediction.
+        Should append self.weak_predictors with a fitted self.weak_learner.
         """
-        weak_predictor = self.weak_learner().fit(X, residue, weights)
-        weak_prediction = np.sign(weak_predictor.predict(X))
-
-        n_examples = X.shape[0]
-        alpha = np.sum(weights * residue * weak_prediction, axis=0)/n_examples/np.mean(weights, axis=0)
-        residue -= alpha * weak_prediction
-
-        self.alphas.append(alpha)
-        self.weak_predictors.append(weak_predictor)
-
-        return residue, weak_prediction
+        raise NotImplementedError
 
 
     def predict(self, X):
         encoded_Y_pred = np.zeros((X.shape[0], self.encoder.encoding_dim)) + self.f0
 
-        for alpha, weak_predictor in zip(self.alphas, self.weak_predictors):
-            encoded_Y_pred += alpha * np.sign(weak_predictor.predict(X))
+        for wp_weight, wp in zip(self.weak_predictors_weights, self.weak_predictors):
+            encoded_Y_pred += wp_weight * wp.predict(X)
 
         return self.encoder.decode_labels(encoded_Y_pred)
 
@@ -103,38 +91,44 @@ class QuadBoostMH:
     
 
     def _compute_coef(self):
-        coefs = [a.reshape(-1,1)*wp.coef_ for a, wp in zip(self.alphas, self.weak_predictors)]
+        coefs = [wp_w.reshape(-1,1)*wp.coef_ for wp_w, wp in zip(self.weak_predictors_weights, self.weak_predictors)]
         coefs = np.sum(coefs, axis=0).reshape((self.encoder.encoding_dim,28,28))
         return coefs
 
 
-class WeakLearner(Ridge):
-    """
-    Linear Ridge regression. Inherits from Ridge of the scikit-learn package.
-    In this implementation, the method 'fit' does not support encoding weights of the QuadBoost algorithm.
-    """
-    def __init__(self, *args, alpha=1, encoder=None, fit_intercept=False, **kwargs):
-        super().__init__(*args, alpha=alpha, fit_intercept=fit_intercept, **kwargs)
-        self.encoder = encoder
-    
-    def fit(self, X, Y, W=None, **kwargs):
-        """
-        Note: this method does not support encoding weights of the QuadBoost algorithm.
-        """
-        X = X.reshape((X.shape[0], -1))
-        if self.encoder != None:
-            Y, W = self.encoder.encode_labels(Y)
-        return super().fit(X, Y, **kwargs)
-    
-    def predict(self, X, **kwargs):
-        X = X.reshape((X.shape[0], -1))
-        return super().predict(X, **kwargs)
 
-    def evaluate(self, X, Y):
-        Y_pred = self.predict(X)
-        if self.encoder != None:
-            Y_pred = self.encoder.decode_labels(Y_pred)
-        return accuracy_score(y_true=Y, y_pred=Y_pred)
+class QuadBoostMH(QuadBoost):
+    def __init__(self, weak_learner, encoder=None):
+        """
+        weak_learner (Object that defines the 'fit' method and the 'predict' method): Weak learner that generates weak predictors to be boosted on.
+        encoder (LabelEncoder object, optional, default=None): Object that encodes the labels to provide an easier separation problem. If None, a one-hot encoding is used.
+        """
+        super().__init__(weak_learner, encoder)
+        self.weak_predictors_weights = []
+
+
+    def _boost(self, X, residue, weights):
+        """
+        Implements one round of boosting. 
+        Appends the lists of weak_predictors and of weak_predictors_weights with the fitted weak learner and its computed weight.
+
+        X (Array of shape (n_examples, ...)): Examples.
+        residue (Array of shape (n_examples, encoding_dim)): Residues to fit for the examples X.
+        weights (Array of shape (n_examples, encoding_dim)): Weights of the examples X for each encoding.
+
+        Returns the calculated residue and the weak_prediction.
+        """
+        weak_predictor = self.weak_learner().fit(X, residue, weights)
+        weak_prediction = weak_predictor.predict(X)
+
+        n_examples = X.shape[0]
+        alpha = np.sum(weights * residue * weak_prediction, axis=0)/n_examples/np.mean(weights, axis=0)
+        residue -= alpha * weak_prediction
+
+        self.weak_predictors_weights.append(alpha)
+        self.weak_predictors.append(weak_predictor)
+
+        return residue, weak_prediction
 
 
 def main():
@@ -143,17 +137,17 @@ def main():
 
     # encoder = LabelEncoder.load_encodings('js_without_0', convert_to_int=True)
     # encoder = LabelEncoder.load_encodings('mario')
-    encoder = OneHotEncoder(Ytr)
-    # encoder = AllPairsEncoder(Ytr)
+    # encoder = OneHotEncoder(Ytr)
+    encoder = AllPairsEncoder(Ytr)
 
 
-    qb = QuadBoostMH(WeakLearner, encoder=encoder)
+    qb = QuadBoostMH(WLRidgeMH, encoder=encoder)
     qb.fit(Xtr, Ytr, T=2)
     acc = qb.evaluate(Xts, Yts)
     print('QB test acc:', acc)
     qb.visualize_coef()
     
-    # wl = WeakLearner(encoder=encoder)
+    # wl = WLRidgeMH(encoder=encoder)
     # wl.fit(Xtr, Ytr)
     # print('WL train acc:', wl.evaluate(Xtr, Ytr))
     # print('WL test acc:', wl.evaluate(Xts, Yts))
