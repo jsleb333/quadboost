cb85ef6ec4d6b8c21e1a311e36e298890a59f78b
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-08-23 14:36:50 -0400

    updated __init__.py to remove stump.py

--- weak_learner/decision_stump/__init__.py
+++ weak_learner/decision_stump/__init__.py
@@ -1,6 +1,4 @@
 try:
-    from stump import *
-    from decision_stump import *
+    from decision_stump import MulticlassDecisionStump
 except ModuleNotFoundError:
-    from weak_learner.decision_stump.stump import *
-    from weak_learner.decision_stump.decision_stump import *
+    from weak_learner.decision_stump.decision_stump import MulticlassDecisionStump
