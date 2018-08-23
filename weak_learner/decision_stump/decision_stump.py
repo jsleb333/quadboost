94ca997dcbc231bccc5dc3d34a73b2fb832ff413
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-08-23 14:33:19 -0400

    merge stump.py into decision_stump.py

--- weak_learner/decision_stump/decision_stump.py
+++ weak_learner/decision_stump/decision_stump.py
@@ -6,8 +6,7 @@ import sys, os
 sys.path.append(os.getcwd())
 
 from weak_learner import Cloner
-from weak_learner.decision_stump import Stump
-from utils import *
+from utils import split_int, timed, ComparableMixin, PicklableExceptionWrapper
 
 
 class MulticlassDecisionStump(Cloner):
@@ -117,6 +116,7 @@ class MulticlassDecisionStump(Cloner):
 
         return sorted_X, sorted_X_idx
 
+
 class StumpFinder:
     """
     Implements the algorithm to find the stump. It is separated from the class MulticlassDecisionStump so that it can be pickled when parallelized with 'multiprocessing' (which uses pickle).
@@ -206,6 +206,51 @@ class StumpFinder:
             normalized_m1 = np.nan_to_num(moments[1]**2/moments[0])
         risks = np.sum(moments[2] - normalized_m1, axis=2) # Shape (n_partitions, n_features)
         return risks
+
+
+class Stump(ComparableMixin, cmp_attr='risk'):
+    """
+    Stump is a simple class that stores the variables used by the MulticlassDecisionStump algorithm. It provides a method 'update' that changes the values only if the new stump is better than the previous one. It also provides a method 'compute_confidence_rates' for the stored stump. Easy comparison between the stumps is provided with the ComparableMixin parent class, which is useful to determine the best stump among many.
+    """
+    def __init__(self, risks, moments):
+        super().__init__()
+        risk = np.sum(risks, axis=0)
+        self.feature = risk.argmin()
+        self.risks = risks[:,self.feature]
+        self.stump_idx = 0
+        self.moment_0 = moments[0,:,self.feature,:].copy()
+        self.moment_1 = moments[1,:,self.feature,:].copy()
+
+    @property
+    def risk(self):
+        return np.sum(self.risks)
+
+    def update(self, risks, moments, possible_stumps, stump_idx):
+        """
+        Updates the current stump with the new stumps only if the new risk is lower than the previous one.
+
+        To optimize the algorithm, the risks are computed only for the acceptable stumps, which happen to be represented as the non zero entries of the variable 'possible_stumps'.
+        """
+        risk = np.sum(risks, axis=0)
+        sparse_feature_idx = risk.argmin()
+        if risk[sparse_feature_idx] < self.risk:
+            self.feature = possible_stumps.nonzero()[0][sparse_feature_idx] # Retrieves the actual index of the feature
+            self.risks = risks[:,sparse_feature_idx]
+            self.moment_0 = moments[0,:,self.feature,:].copy()
+            self.moment_1 = moments[1,:,self.feature,:].copy()
+            self.stump_idx = stump_idx
+
+    @property
+    def confidence_rates(self):
+        return np.divide(self.moment_1, self.moment_0, where=self.moment_0!=0)
+
+    def compute_stump_value(self, sorted_X):
+        feat_val = lambda idx: sorted_X[idx, self.feature]
+        if self.stump_idx != 0:
+            self.stump = (feat_val(self.stump_idx) + feat_val(self.stump_idx-1))/2
+        else:
+            self.stump = feat_val(self.stump_idx) - 1
+        return self.stump
 
 
 @timed
