bc6ce76c77fb929f7ef3a318aa73da75c82a8d7e
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-08-30 16:21:24 -0400

    cpp stump implementation

--- weak_learner/decision_stump/src/stump.cc
+++ weak_learner/decision_stump/src/stump.cc
@@ -3,6 +3,10 @@
 #include "xtensor/xarray.hpp"
 #include "xtensor/xmath.hpp"
 #include "xtensor/xsort.hpp"
+#include "xtensor/xoperation.hpp"
+#include "xtensor/xeval.hpp"
+#include "xtensor/xview.hpp"
+#include "xtensor/xbuilder.hpp"
 #include "stump.h"
 
 namespace decision_stump
@@ -14,9 +18,52 @@ namespace decision_stump
 Stump::Stump(const xt::xarray<double> &risks, const xt::xarray<double> &moments)
 {
     auto risk = xt::sum(risks, {0});
-    feature_ = xt::argmin(risk);
+    feature_ = xt::argmin(risk)(0);
+    risks_ = xt::view(risks, xt::all(), feature_);
+
+    moments_0_ = xt::xarray<double>(xt::view(moments, 0, xt::all(), feature_, xt::all()));
+    moments_1_ = xt::xarray<double>(xt::view(moments, 1, xt::all(), feature_, xt::all()));
 }
 
+/*************
+ * Accessors
+ *************/
+const xt::xarray<double> Stump::get_confidence_rates() const
+{   
+    auto safe_divisions = xt::not_equal(moments_0_, xt::zeros_like(moments_0_));
+    return xt::where(safe_divisions, moments_0_ / moments_1_, moments_0_);
+}
 
+/******************
+ * Public methods
+ ******************/
+void Stump::compute_stump_value(xt::xarray<double>& sorted_X)
+{
+    if (stump_idx_ != 0)
+    {
+        stump_ = ( sorted_X(stump_idx_, feature_) + sorted_X(stump_idx_-1, feature_) )/2;
+    }
+    else
+    {
+        stump_ = sorted_X(stump_idx_, feature_) - 1;
+    }
+}
+
+void Stump::update(xt::xarray<double>& risks,
+            xt::xarray<double>& moments,
+            xt::xarray<size_t>& possible_stumps,
+            size_t stump_idx)
+{
+    auto risk = xt::sum(risks, {0});
+    size_t sparse_feature_idx = xt::argmin(risk)(0);
+    if (risk(sparse_feature_idx) < get_risk())
+    {
+        feature_ = xt::nonzero(possible_stumps)[sparse_feature_idx][0];
+        risks_ = xt::xarray<double>(xt::view(risks, xt::all(), sparse_feature_idx));
+        moments_0_ = xt::xarray<double>(xt::view(moments, 0, xt::all(), feature_, xt::all()));
+        moments_1_ = xt::xarray<double>(xt::view(moments, 1, xt::all(), feature_, xt::all()));
+        stump_idx_ = stump_idx;
+    }
+}
 
 }
--- weak_learner/decision_stump/src/stump.h
+++ weak_learner/decision_stump/src/stump.h
@@ -16,7 +16,8 @@ public:
     Stump(const xt::xarray<double>& risks, const xt::xarray<double>& moments);
 
     // Accessors
-    const double get_risk() const;
+    const double get_risk() const
+        {return xt::sum(risks_)(0);}
     const xt::xarray<double> get_risks() const
         {return risks_;}
     const double get_stump() const
@@ -25,25 +26,23 @@ public:
         {return stump_idx_;}
     const size_t get_feature() const
         {return feature_;}
-    const double get_confidence_rates() const;
+    const xt::xarray<double> get_confidence_rates() const;
 
     // Public methods
     void compute_stump_value(xt::xarray<double>& sorted_X);
     void update(xt::xarray<double>& risks,
                 xt::xarray<double>& moments,
                 xt::xarray<size_t>& possible_stumps,
-                xt::xarray<size_t>& stump_idx);
+                size_t stump_idx);
 
 private:
     // Attributes
     xt::xarray<double> risks_;
+    xt::xarray<double> moments_0_;
+    xt::xarray<double> moments_1_;
     double stump_;
-    size_t stump_idx_;
+    size_t stump_idx_ = 0;
     size_t feature_;
-
-    // Utilitary methods
-    void _update_moments(xt::xarray<double>& moments, xt::xarray<int>& row_idx);
-    const xt::xarray<double>& _compute_risks(const xt::xarray<double>& moments) const;
 };
 
 } // namespace decision_stump
