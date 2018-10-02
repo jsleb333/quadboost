2d626efde019e034c30d5c20a31ab6eddbad92d4
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-10-02 18:34:55 -0400

    fix bug where timed could not be used with methods

--- utils/timed.py
+++ utils/timed.py
@@ -12,9 +12,9 @@ except ModuleNotFoundError:
     Style = EmptyStringAttrClass()
 
 
-class timed:
+class _ColoredTimer:
     """
-    Wrapper class to time a function and display it in a colorful fashion. The wrapper takes optional keyword arguments to customize the display.
+    Wrapper class to time a function and display it in a colorful fashion. The wrapper takes optional keyword arguments to customize the display. This class should be used through the 'timed' function. (Necessary for it to work on methods too.)
     """
     def __init__(self, func=None, *, datetime_format='%Y-%m-%d %Hh%Mm%Ss',
                  display_func_name=True,
@@ -25,7 +25,7 @@ class timed:
                  datetime_color='LIGHTMAGENTA_EX'):
         """
         Args:
-            func (callable or None): Function or method to time.
+            func (callable or None): Function to time.
             datetime_format (str or None, optional): Datetime format used to display the date and time. The format follows the template of the 'datetime' package. If None, no date or time will be displayed.
             display_func_name (bool): Whether or not the function name should be displayed.
             main_color (str): Color in which the main text will be displayed. Choices are those from the package colorama.
@@ -56,22 +56,29 @@ class timed:
             ...     print('bar!')
             ...     raise RuntimeError
             ...
-            >>> bar()
+            >>> try:
+            ...     bar()
+            ... except RuntimeError: pass
             Execution started on 20h25m06s.
 
             bar!
 
             Execution terminated after 0.00 seconds on 20h25m06s.
+        
+        Example 3:
+            >>> class Spam:
+            ...     @timed
+            ...     def spam(self):
+            ...         print('egg!')
 
-            Traceback (most recent call last):
-            File "...", line ..., in module bar()
-            ...
-                raise RuntimeError
-            RuntimeError
+            >>> Spam().spam()
+            Execution of 'spam' started on 2018-10-02 18h33m14s.
+
+            egg!
+
+            Execution of 'spam' completed in 0.00 seconds on 2018-10-02 18h33m14s.
         """
         self.func = func
-        if func is not None:
-            functools.update_wrapper(self, self.func)
         self.start_time = None
         self.datetime_format = datetime_format
         self.display_func_name = display_func_name
@@ -83,15 +90,6 @@ class timed:
         self.datetime_color = getattr(Fore, datetime_color)
 
     def __call__(self, *args, **kwargs):
-        if self.func is None:
-            # self.func is None if timed was initialized with keyword arguments. In that case, the first call to __call__ is to initialize the function to wrap.
-            self.func, = args
-            functools.update_wrapper(self, self.func)
-            return self
-        else:
-            return self.wrapper(*args, **kwargs)
-
-    def wrapper(self, *args, **kwargs):
         """
         Actual method that wraps.
         """
@@ -139,6 +137,23 @@ class timed:
             + Style.RESET_ALL)
 
 
+@functools.wraps(_ColoredTimer.__init__)
+def timed(func=None, **kwargs):
+    """
+    See _ColoredTimer __init__ documentation for arguments and usage examples. This code bit only exists because it is necessary for methods to be correctly wrapped. (Else, the reference contained in 'self' is a _ColoredTimer instance instead of the actual owner of the method.)
+    """
+    if func is None:
+        def missing_func_timed(new_func):
+            return timed(new_func, **kwargs)
+        return missing_func_timed
+
+    wrapper = _ColoredTimer(func, **kwargs)
+    @functools.wraps(func)
+    def timed_func(*args, **kwargs): # args[0] is the reference to 'self' if 'func' is a method.
+        return wrapper(*args, **kwargs)
+    return timed_func
+
+
 def old_timed(func):
     # For reference, this is an old simpler version of timed.
     @functools.wraps(func)
@@ -167,4 +182,13 @@ if __name__ == '__main__':
     def bar():
         print('bar!')
         raise RuntimeError
-    bar()
+    try:
+        bar()
+    except RuntimeError: pass
+
+    class Spam:
+        @timed
+        def spam(self):
+            print('egg!')
+
+    Spam().spam()
