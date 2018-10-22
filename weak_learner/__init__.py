65a92f4ab5277d236c74a47068cace69a1c0d47a
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-10-22 11:40:57 -0400

    added RandomFilters to module

--- weak_learner/__init__.py
+++ weak_learner/__init__.py
@@ -2,11 +2,13 @@ try:
     from weak_learner.weak_learner_base import WeakLearnerBase, Cloner
     from weak_learner.ridge import *
     from weak_learner.svr import *
+    from weak_learner.random_filters import RandomFilters
     from weak_learner.decision_stump import MulticlassDecisionStump
     from weak_learner.decision_tree import MulticlassDecisionTree
 except ModuleNotFoundError:
     from weak_learner_base import WeakLearnerBase, Cloner
     from ridge import *
     from svr import *
+    from random_filters import RandomFilters
     from decision_stump import MulticlassDecisionStump
     from decision_tree import MulticlassDecisionTree
