293ed97b23ca9c70e96a341639831569e8a19e6d
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-10-30 15:19:18 -0400

    updated main with new weak learner

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from quadboost import QuadBoostMHCR
 from callbacks import ModelCheckpoint, CSVLogger, BreakOnZeroRiskCallback
 from label_encoder import LabelEncoder, OneHotEncoder, AllPairsEncoder
 from mnist_dataset import MNISTDataset
-from weak_learner import WLRidge, WLThresholdedRidge, MulticlassDecisionStump, MulticlassDecisionTree
+from weak_learner import WLRidge, WLThresholdedRidge, MulticlassDecisionStump, MulticlassDecisionTree, RandomFilters
 import logging
 
 
 @timed
 @parse
-def main(m=60_000, dataset='haar_mnist', encodings='onehot', wl='dt', n_jobs=1, max_n_leaves=4, max_round=600, patience=10, resume=0):
+def main(m=60_000, dataset='haar_mnist', encodings='onehot', wl='dt', n_jobs=1, max_n_leaves=4, max_round=1000, patience=1000, resume=0, n_filters=3, kernel_size=5):
     ### Data loading
     mnist = MNISTDataset.load(dataset+'.pkl')
     (Xtr, Ytr), (Xts, Yts) = mnist.get_train_test(center=False, reduce=False)
@@ -26,6 +26,7 @@ def main(m=60_000, dataset='haar_mnist', encodings='onehot', wl='dt', n_jobs=1, 
             encoder = LabelEncoder({int(label):encoding for label, encoding in encoder.labels_encoding.items()})
 
     ### Choice of weak learner
+    kwargs = {}
     if wl == 'ds' or 'decision-stump':
         weak_learner = MulticlassDecisionStump()
         kwargs = dict(zip(('sorted_X', 'sorted_X_idx'), weak_learner.sort_data(Xtr[:m])))
@@ -36,7 +37,9 @@ def main(m=60_000, dataset='haar_mnist', encodings='onehot', wl='dt', n_jobs=1, 
         kwargs['n_jobs'] = n_jobs
     elif wl == 'ridge':
         weak_learner = WLThresholdedRidge(threshold=.5)
-        kwargs = {}
+    elif wl == 'rf' or 'random_filters':
+        kernel_size = (kernel_size, kernel_size)
+        weak_learner = RandomFilters(n_filters=n_filters, kernel_size=kernel_size)
 
     ### Callbacks
     filename = f'd={dataset}-e={encodings}-wl={wl}-'
