bcb4ea29c4ba993dbf7791be4a0ab4ada294fa69
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-10-31 13:57:15 -0400

    merge Cloner with WeakLearnerBase

--- weak_learner/__init__.py
+++ weak_learner/__init__.py
@@ -1,12 +1,12 @@
 try:
-    from weak_learner.weak_learner_base import WeakLearnerBase, Cloner
+    from weak_learner.weak_learner_base import WeakLearnerBase
     from weak_learner.ridge import *
     from weak_learner.svr import *
     from weak_learner.random_filters import RandomFilters
     from weak_learner.decision_stump import MulticlassDecisionStump
     from weak_learner.decision_tree import MulticlassDecisionTree
 except ModuleNotFoundError:
-    from weak_learner_base import WeakLearnerBase, Cloner
+    from weak_learner_base import WeakLearnerBase
     from ridge import *
     from svr import *
     from random_filters import RandomFilters
--- weak_learner/weak_learner_base.py
+++ weak_learner/weak_learner_base.py
@@ -2,21 +2,19 @@ from sklearn.metrics import accuracy_score
 import inspect
 
 
-class Cloner:
+class WeakLearnerBase:
     """
-    This constructor class makes any weak learners clonable by setting the __call__ function as a constructor using the initialization parameters.
-    This class, to be inherited, acts like a decorator around the constructor, but without the inconveniences of decorators (with pickling).
+    This class implements a abstract base weak learner that should be inherited. It makes sure all weak learner are clonable and have an encoder, as well as a predict and an evaluate methods.
     """
     def __new__(cls, *args, **kwargs):
+        """
+        This constructor class makes any weak learners clonable by setting the __call__ function as a constructor using the initialization parameters.
+        This class, to be inherited, acts like a decorator around the constructor, but without the inconveniences of decorators (with pickling).
+        """
         def clone(self): return cls(*args, **kwargs)
         cls.__call__ = clone
         return super().__new__(cls)
 
-
-class WeakLearnerBase(Cloner):
-    """
-    This class implements a abstract base weak learner that should be inherited. It makes sure all weak learner have an encoder and an evaluate method.
-    """
     def __init__(self, *args, encoder=None, **kwargs):
         self.encoder = encoder
         super().__init__(*args, **kwargs)
