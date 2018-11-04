d0b759eefd8c74a75581ae54096452c8ac48d4ce
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-11-04 15:44:53 -0500

    local convolution in init

--- weak_learner/__init__.py
+++ weak_learner/__init__.py
@@ -5,6 +5,7 @@ try:
     from weak_learner.random_filters import RandomFilters
     from weak_learner.decision_stump import MulticlassDecisionStump
     from weak_learner.decision_tree import MulticlassDecisionTree
+    from weak_learner.local_convolution import LocalConvolution
 except ModuleNotFoundError:
     from weak_learner_base import _WeakLearnerBase
     from ridge import *
@@ -12,3 +13,4 @@ except ModuleNotFoundError:
     from random_filters import RandomFilters
     from decision_stump import MulticlassDecisionStump
     from decision_tree import MulticlassDecisionTree
+    from local_convolution import LocalConvolution
