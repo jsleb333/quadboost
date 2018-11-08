21b0a1f13a527c3e9bba18b45d06324b75ed611e
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-11-08 13:13:02 -0500

    wip filter bank

--- weak_learner/random_filters.py
+++ weak_learner/random_filters.py
@@ -30,7 +30,7 @@ class RandomFilters(_WeakLearnerBase):
     """
     This weak learner is takes random filters and convolutes the dataset with them. It then applies a non-linearity on the resulting random features and makes a Ridge regression as final classifier.
     """
-    def __init__(self, encoder=None, n_filters=2, kernel_size=(5,5), init_filters='random', filter_normalization=None):
+    def __init__(self, encoder=None, n_filters=2, kernel_size=(5,5), init_filters='random', filter_normalization=None, filter_bank=None):
         """
         Args:
             encoder (LabelEncoder object, optional): Encoder to encode labels. If None, no encoding will be made before fitting.
@@ -38,15 +38,17 @@ class RandomFilters(_WeakLearnerBase):
             kernel_size ((int, int), optional): Size of the filters.
             init_filters (str, either 'random' or 'from_data'): Choice of initialization of the filters weights. If 'random', the weights are drawn from a normal distribution. If 'from_data', the weights are taken as patches from the data, uniformly drawn.
             filter_normalization (str or None, either 'c', 'r', 'n', 'cr' or 'cn'): If 'c', weights of the filters will be centered (i.e. of mean 0), if 'r', they will be reduced (i.e. of unit standard deviation), if 'n' they will be normalized (i.e. of unit euclidean norm) and 'cr' and 'cn' are combinations. If 'n' combined with 'r', the 'r' flag prevails.
+            filter_bank (Array or None, optional): Bank of images for filters to be drawn. Only valid if 'init_filters' is set to 'from_bank'.
         """
         self.encoder = encoder
         self.n_filters = n_filters
         self.kernel_size = kernel_size
         self.filter_normalization = filter_normalization or ''
+        self.filter_bank = filter_bank
 
         if init_filters == 'random':
             self.init_filters = None # Already random by default
-        elif init_filters == 'from_data':
+        elif init_filters == 'from_data' or init_filters == 'from_bank':
             self.init_filters = self.init_from_images
         else:
             raise ValueError(f"'{init_filters} is an invalid init_filters option.")
@@ -63,11 +65,12 @@ class RandomFilters(_WeakLearnerBase):
         with torch.no_grad():
             if self.encoder is not None:
                 Y, W = self.encoder.encode_labels(Y)
-            X = self._format_X(X)
+            X = self._format_data(X)
 
             self.filters = Filters(self.n_filters, self.kernel_size)
             if self.init_filters:
-                self.init_filters(X=X, filter_normalization=self.filter_normalization)
+                filter_bank = self.filter_bank or X
+                self.init_filters(filter_bank, filter_normalization=self.filter_normalization)
 
             random_feat = self.filters(X).numpy().reshape((X.shape[0], -1))
 
@@ -77,11 +80,11 @@ class RandomFilters(_WeakLearnerBase):
 
         return self
 
-    def _format_X(self, X):
-        X = torch.from_numpy(X).float()
-        if len(X.shape) == 3:
-            X = torch.unsqueeze(X, dim=1)
-        return X
+    def _format_data(self, data):
+        data = torch.from_numpy(data).float()
+        if len(data.shape) == 3:
+            data = torch.unsqueeze(data, dim=1)
+        return data
 
     def predict(self, X):
         """
@@ -90,19 +93,19 @@ class RandomFilters(_WeakLearnerBase):
         Args:
             X (Array of shape (n_examples, ...)): Examples to predict.
         """
-        X = self._format_X(X)
+        X = self._format_data(X)
 
         random_feat = self.filters(X).numpy().reshape((X.shape[0], -1))
 
         return self._classifier.predict(random_feat)
 
-    def init_from_images(self, X, filter_normalization='', **kwargs):
+    def init_from_images(self, filter_bank, filter_normalization='', **kwargs):
         """
         Assumes X is a torch tensor with shape (n_examples, n_channels, width, height).
         """
         weights = []
         for _ in range(self.n_filters):
-            weight = self._draw_from_images(X)
+            weight = self._draw_from_images(self._format_data(filter_bank))
             if 'c' in filter_normalization:
                 weight -= torch.mean(weight)
             if 'n' in filter_normalization:
