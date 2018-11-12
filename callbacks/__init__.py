661f4e2f527dad46d24f72f31fd3d619417d0c83
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2018-11-12 14:50:49 -0500

    new restore best callback

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -7,6 +7,7 @@ try:
     from callbacks_manager_iterator import *
     from model_checkpoint import *
     from logger import *
+    from restore import RestoreBestModelCallback
 except ModuleNotFoundError:
     from callbacks.callback import *
     from callbacks.break_callbacks import *
@@ -16,3 +17,4 @@ except ModuleNotFoundError:
     from callbacks.callbacks_manager_iterator import *
     from callbacks.model_checkpoint import *
     from callbacks.logger import *
+    from callbacks.restore import RestoreBestModelCallback
