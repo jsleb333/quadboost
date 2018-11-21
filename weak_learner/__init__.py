9264c22ca0351873d1c98c452f461d73e7c8de8b
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-11-20 22:06:21 -0500

    making all importable

--- weak_learner/__init__.py
+++ weak_learner/__init__.py
@@ -4,11 +4,11 @@ try:
     from weak_learner.svr import *
     from weak_learner.decision_stump import MulticlassDecisionStump
     from weak_learner.decision_tree import MulticlassDecisionTree
-    from weak_learner.random_convolution import RandomConvolution
+    from weak_learner.random_convolution import RandomConvolution, Filters, LocalFilters, WeightFromBankGenerator, center_weight, normalize_weight, reduce_weight
 except ModuleNotFoundError:
     from weak_learner_base import _WeakLearnerBase, _Cloner
     from ridge import *
     from svr import *
     from decision_stump import MulticlassDecisionStump
     from decision_tree import MulticlassDecisionTree
-    from random_convolution import RandomConvolution
+    from random_convolution import RandomConvolution, Filters, LocalFilters, WeightFromBankGenerator, center_weight, normalize_weight, reduce_weight
