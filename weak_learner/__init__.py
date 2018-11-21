fd834b18644cffbc8e5f2471a09599b527b41f8c
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-11-20 20:17:23 -0500

    made _Cloner importable

--- weak_learner/__init__.py
+++ weak_learner/__init__.py
@@ -1,12 +1,12 @@
 try:
-    from weak_learner.weak_learner_base import _WeakLearnerBase
+    from weak_learner.weak_learner_base import _WeakLearnerBase, _Cloner
     from weak_learner.ridge import *
     from weak_learner.svr import *
     from weak_learner.decision_stump import MulticlassDecisionStump
     from weak_learner.decision_tree import MulticlassDecisionTree
     from weak_learner.random_convolution import RandomCompleteConvolution, RandomLocalConvolution
 except ModuleNotFoundError:
-    from weak_learner_base import _WeakLearnerBase
+    from weak_learner_base import _WeakLearnerBase, _Cloner
     from ridge import *
     from svr import *
     from decision_stump import MulticlassDecisionStump
