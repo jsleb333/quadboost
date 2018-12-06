a3662341b678990f7eb87a18ae73983500cba618
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2018-12-06 17:26:17 -0500

    removed hook in RandomAffine

--- quadboost/data_preprocessing/data_augmentation.py
+++ quadboost/data_preprocessing/data_augmentation.py
@@ -3,17 +3,17 @@ import torch
 from torchvision.transforms import RandomAffine
 import torchvision.transforms.functional as tf
 import torch
-def my_call(self, img):
-    """
-        img (PIL Image): Image to be transformed.
+# def my_call(self, img):
+#     """
+#         img (PIL Image): Image to be transformed.
 
-    Returns:
-        Tuples of params.
-        PIL Image: Affine transformed image.
-    """
-    ret = self.get_params(self.degrees, self.translate, self.scale, self.shear, img.size)
-    return ret, tf.affine(img, *ret, resample=self.resample, fillcolor=self.fillcolor)
-RandomAffine.__call__ = my_call
+#     Returns:
+#         Tuples of params.
+#         PIL Image: Affine transformed image.
+#     """
+#     ret = self.get_params(self.degrees, self.translate, self.scale, self.shear, img.size)
+#     return ret, tf.affine(img, *ret, resample=self.resample, fillcolor=self.fillcolor)
+# RandomAffine.__call__ = my_call
 
 
 from PIL.Image import BICUBIC
@@ -35,7 +35,8 @@ def extend_mnist(Xtr, Ytr, N=1000, degrees=15, scale=(.85,1.11), shear=15):
         idx = np.random.randint(Xtr.shape[0])
         X = Xtr_torch[idx]
         X_pil = tf.pad(tf.to_pil_image(X),3)
-        params, X_transform = AffineTransform(X_pil)
+        # params, X_transform = AffineTransform(X_pil)
+        X_transform = AffineTransform(X_pil)
         X_transform = tf.to_tensor(tf.crop(X_transform, 3, 3, 28, 28)).numpy().reshape(28,28)
         # trans_title = f'trans-d={params[0]:.2f}-scale={params[2]:.2f}-shear={params[3]:.2f}'
         # plot_images([Xtr[_].reshape(28,28), X_transform], ['orig', trans_title])
