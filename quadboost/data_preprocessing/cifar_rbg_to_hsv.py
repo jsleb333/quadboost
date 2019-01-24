a611ddf8ee7cfb14612367a40d1dce7ab45907dc
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2019-01-24 16:40:54 -0500

    grey and without hue cifar

--- quadboost/data_preprocessing/cifar_rbg_to_hsv.py
+++ quadboost/data_preprocessing/cifar_rbg_to_hsv.py
@@ -9,25 +9,37 @@ from quadboost.datasets import MNISTDataset, CIFAR10Dataset
 from quadboost.utils import make_fig_axes
 
 
-cifar = CIFAR10Dataset.load()
-(Xtr, Ytr), (Xts, Yts) = cifar.get_train_test(shuffle=False)
+# cifar = CIFAR10Dataset.load()
+# (Xtr, Ytr), (Xts, Yts) = cifar.get_train_test(shuffle=False)
 
-def convert_to_hsv(X):
-    X_r, X_g, X_b = (X[:,i,:,:,np.newaxis] for i in range(3))
-    X_hsv = np.zeros_like(X, dtype=np.float32)
-    X_channels_at_the_end = np.concatenate((X_r, X_g, X_b), axis=3)/255
-    for i, x in enumerate(X_channels_at_the_end):
-        pic = rgb2hsv(x)
-        h, s, v = (pic[np.newaxis,:,:,i] for i in range(3))
-        pic = np.concatenate((h,s,v), axis=0)
-        X_hsv[i] = pic
-    return X_hsv
+# def convert_to_hsv(X):
+#     X_r, X_g, X_b = (X[:,i,:,:,np.newaxis] for i in range(3))
+#     X_hsv = np.zeros_like(X, dtype=np.float32)
+#     X_channels_at_the_end = np.concatenate((X_r, X_g, X_b), axis=3)/255
+#     for i, x in enumerate(X_channels_at_the_end):
+#         pic = rgb2hsv(x)
+#         h, s, v = (pic[np.newaxis,:,:,i] for i in range(3))
+#         pic = np.concatenate((h,s,v), axis=0)
+#         X_hsv[i] = pic
+#     return X_hsv
 
-Xtr_hsv = convert_to_hsv(Xtr)
-Xts_hsv = convert_to_hsv(Xts)
+# Xtr_hsv = convert_to_hsv(Xtr)
+# Xts_hsv = convert_to_hsv(Xts)
 
-cifar_hsv = CIFAR10Dataset(Xtr_hsv, Ytr, Xts_hsv, Yts)
-cifar_hsv.save('cifar10hsv.pkl')
+# cifar_hsv = CIFAR10Dataset(Xtr_hsv, Ytr, Xts_hsv, Yts)
+# cifar_hsv.save('cifar10hsv.pkl')
+
+
+cifar_hsv = CIFAR10Dataset.load('cifar10hsv.pkl')
+(Xtr, Ytr), (Xts, Yts) = cifar_hsv.get_train_test(shuffle=False)
+
+cifar_grey = CIFAR10Dataset(Xtr[:,2], Ytr, Xts[:,2], Yts, shape=(32,32))
+print(cifar_grey.Xtr.shape)
+cifar_grey.save('cifar10grey.pkl')
+
+cifar_sv = CIFAR10Dataset(Xtr[:,1:], Ytr, Xts[:,1:], Yts, shape=(2,32,32))
+print(cifar_sv.Xtr.shape)
+cifar_sv.save('cifar10sv.pkl')
 
 
 
