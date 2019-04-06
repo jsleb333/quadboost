1fdba7ee54269be3fb4815cfd4f5b87aad60fafd
jsleb333 <jean-samuel.leboeuf.1@ulaval.ca>
2019-04-06 15:16:21 -0400

    script to plot

---
+++ rrc-features_plotting.py
@@ -0,0 +1,101 @@
+import torch
+import logging
+
+from quadboost.label_encoder import OneHotEncoder
+from quadboost.weak_learner import *
+from quadboost.datasets import MNISTDataset, CIFAR10Dataset
+from quadboost.utils import parse, timed
+
+from py2tex import Document, Plot
+
+seed = 101
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+mnist = MNISTDataset.load()
+(Xtr, Ytr), (Xts, Yts) = mnist.get_train_test(center=True, reduce=True)
+Xtr = torch.unsqueeze(torch.from_numpy(Xtr), dim=1)
+Xts = torch.unsqueeze(torch.from_numpy(Xts), dim=1)
+encoder = OneHotEncoder(Ytr)
+
+print('CUDA')
+Xtr = Xtr.to(device='cuda:0')
+Xts = Xts.to(device='cuda:0')
+
+
+def n_features_plot(nf,
+                    m=1000,
+                    bank=3000,
+                    nt=10,
+                    rotation=0,
+                    scale=(1,1),
+                    shear=0
+                    ):
+    seed = 101
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    print(f'n filters = {nf}, n transform = {nt}')
+    filter_gen = WeightFromBankGenerator(filter_bank=Xtr[-bank:],
+                                        filters_shape=(11,11),
+                                        # filters_shape=(5,5),
+                                        # filters_shape_high=(16,16),
+                                        margin=2,
+                                        filter_processing=[center_weight],
+                                        rotation=rotation,
+                                        scale=scale,
+                                        shear=shear,
+                                        n_transforms=nt,
+                                        )
+    filters = LocalFilters(n_filters=nf,
+                        maxpool_shape=(-1,-1,-1),
+                        weights_generator=filter_gen,
+                        locality=3,
+                        )
+    weak_learner = Ridge
+    # weak_learner = MulticlassDecisionStump
+
+    print('Starting fit')
+    wl = RandomConvolution(filters=filters,
+                        weak_learner=weak_learner,
+                        encoder=encoder,
+                        )
+    # wl = WLRidge(encoder=encoder)
+    wl.fit(Xtr[:m], Ytr[:m])
+    tr_acc = wl.evaluate(Xtr[:m], Ytr[:m])
+    ts_acc = wl.evaluate(Xts[:m], Yts[:m])
+    print('Train acc', tr_acc)
+    print('Test acc', ts_acc)
+
+    return tr_acc, ts_acc
+
+
+if __name__ == '__main__':
+    nt = 40
+    # scale = (1,1)
+    # shear = 0
+    # rotation = 0
+    scale = (0.9, 1.11)
+    shear = 10
+    rotation = 15
+
+    doc_name = f'n_features_plot_n_transforms={nt}'
+    doc = Document(doc_name)
+    plot = doc.new(Plot(plot_name=doc_name))
+
+    nfs = list(np.linspace(10,90,9)) + list(np.linspace(100,700,7)) + [784,800,900,1000]
+    # nfs = [10,20,30]
+    tr_accs, ts_accs = [], []
+    for nf in nfs:
+        tr_acc, ts_acc = n_features_plot(nf, m=57000, nt=nt, rotation=rotation, scale=scale, shear=shear)
+        tr_accs.append(tr_acc)
+        ts_accs.append(ts_acc)
+
+    plot.add_plot(nfs, tr_accs)
+    plot.add_plot(nfs, ts_accs)
+
+    plot.y_min = 0
+    plot.y_max = 1
+
+    doc.build()
+
